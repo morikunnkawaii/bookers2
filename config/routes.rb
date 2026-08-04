c9d0d509c2f123fc83cc92090abb26728557e351
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-04 13:54:48 +0000

    [add]rails認証機能導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root to: "home#top"
   get "home/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
