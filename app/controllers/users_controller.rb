a54995544fe9136e8bb6aa571f144829fd022bd8
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-05 04:56:29 +0000

    [add]user controllerとuser nameカラム追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path #後で変更するよユーザー詳細画面へ
+      redirect_to new_session_path notice: "successfully"#後で変更するよユーザー詳細画面へ
     else
       render :new, status: :unprocessable_entity
     end
