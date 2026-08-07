9767cce53eb156f296cf59b70d2844dc6e64024b
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-07 11:42:01 +0000

    [add]userにupdate追加　名前しか変えれてないから記述必要

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path
+  end
+
   private
 
   def user_params
