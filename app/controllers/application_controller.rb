37e221be041c013b3e53946bbdb1e4fe6b79d1ce
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-10 09:39:40 +0000

    [add]link login先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   private
 
+  def after_authentication_url
+    user_path(Current.user.id)#user showへ
+  end
+
   def after_logout_url
     root_path
   end
