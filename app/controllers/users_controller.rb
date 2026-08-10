087398646d8208cb1500490ec98b7706dec80193
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-10 16:04:22 +0000

    [add]バリテーション完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    flash[:notice] = "You have updated user successfully"
-    redirect_to user_path
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
