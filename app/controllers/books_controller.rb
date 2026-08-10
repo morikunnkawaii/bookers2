7bd40448d03188dc0b02eb78f9bd82aa86a4d50a
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-10 14:28:32 +0000

    [add]book新規投稿にバリテーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = Current.user.id
-    @book.save
-    flash[:notice] = "You have created book successfully"
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def show
