150d97fa98de83d7fb55499c4f0fedc011b1b547
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-12 19:39:28 +0000

    [add]部分テンプレート追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,12 +18,15 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = Current.user
+    @booknew = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = @user
+    @booknew = Book.new #どうやらアソシエーションのおかげか、book.newができる
   end
 
   def edit
