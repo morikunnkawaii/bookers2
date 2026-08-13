25ae0e5aa01534a4ff4a64d25797b3d9db8d4734
Ubuntu <ubuntu@ip-172-31-36-205.ap-northeast-1.compute.internal>
2026-08-13 18:02:05 +0000

    [add]passwordのバリテーションが被った為にエラー発生。それを直した。destroyするときに本当に削除するかメッセージが出るように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20}
   validates :introduction, length: {maximum: 50}
   validates :email_address, presence: true
-  validates :password, presence: true, length: { minimum: 6}
+  validates :password, length: { minimum: 6}
 
   def get_profile_image(width, height)
     unless profile_image.attached?
